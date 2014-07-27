8bf2e3e3b9c45b4b93f0162bb895a4f8ee6e4a18
Luke Davis <luke@peopleofthebit.com>
2014-07-26 23:47:59 -0400

    First pass at the 7 ingredients in Go. Very buggy. Do not use.

--- si_go.go
+++ si_go.go
@@ -14,10 +14,37 @@ var c bool = false
 
 //CONDITIONALS
 
+func sqrt(x float64) string {
+    if x < 0 {
+        return sqrt(-x) + "i"
+    }
+    return fmt.Sprint(math.Sqrt(x))
+}
+
 //SETS
 
+var x [2]string
+x[0] = "Hello"
+x[1] = "World"
+    
+    
 //LOOPS
 
+func looper () int {
+ sum := 0
+    for i := 0; i < 10; i++ {
+        sum += i
+    }
+ return sum
+}
+
+func alooper () string {
+  d:=""
+    for i := 0; i < len(p); i++ {
+        d+=("p[%d] == %d\n", i, p[i])
+    }
+  return d
+}
 //FUNCTIONS
 
 func ichi() int {
