de7bffbbbc6847b8df3aecffd2f236a991852d53
Luke Davis <luke@peopleofthebit.com>
2014-07-26 23:27:56 -0400

    Create si_go.go

---
+++ si_go.go
@@ -0,0 +1,30 @@
+//Seven Ingredients: Go
+
+package main
+
+import "fmt"
+
+
+
+//VARIABLES
+
+var a int = 10
+var b string = "some words"
+var c bool = false
+
+//CONDITIONALS
+
+//SETS
+
+//LOOPS
+
+//FUNCTIONS
+
+func ichi() int {
+	var d int = 10
+	return d
+	}
+	
+func main() {
+	fmt.Println( ichi() )
+}
